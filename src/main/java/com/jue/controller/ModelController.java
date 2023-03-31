4d43266b15be0c0b0445fc4d15d6390d569e5870
Pride <3110521267@qq.com>
2023-03-31 15:10:24 +0800

    Changes

--- src/main/java/com/jue/controller/ModelController.java
+++ src/main/java/com/jue/controller/ModelController.java
@@ -4,7 +4,6 @@ import com.jue.domain.Model;
 import com.jue.service.impl.ModelServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 /**
