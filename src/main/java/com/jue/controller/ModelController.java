58312a2f9dd6f30d65ceb871c95809ab836b3e71
Pride <3110521267@qq.com>
2023-04-11 11:45:52 +0800

    Changes

--- src/main/java/com/jue/controller/ModelController.java
+++ src/main/java/com/jue/controller/ModelController.java
@@ -15,26 +15,50 @@ public class ModelController {
     @Autowired
     private ModelServiceImpl modelService;
 
+    /**
+     * 获取所有信息
+     * @return
+     */
     @GetMapping
     public List<Model> getAll() {
         return modelService.getAll();
     }
 
+    /**
+     * 添加一条数据
+     * @param model
+     * @return
+     */
     @PostMapping
     public boolean save(@RequestBody Model model) {
         return modelService.save(model);
     }
 
+    /**
+     * 根据实体修改信息
+     * @param model
+     * @return
+     */
     @PutMapping
     public boolean update(@RequestBody Model model) {
         return modelService.modify(model);
     }
 
+    /**
+     * 根据id删除记录
+     * @param id
+     * @return
+     */
     @DeleteMapping("/{id}")
     public boolean delete(@PathVariable Integer id) {
         return modelService.delete(id);
     }
 
+    /**
+     * 根据id查询记录
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
     public Model getById(@PathVariable Integer id){
         System.out.println();
